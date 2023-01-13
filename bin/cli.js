3a3e37cd98ce465eb4bc0dd9ef9f9138ab67271a
Frans J Titulaer <fransjlt@gmail.com>
2023-01-13 14:31:01 +0100

    small fix

--- bin/cli.js
+++ bin/cli.js
@@ -12,20 +12,20 @@ const runCommand = (command) => {
   return true;
 };
 
-const repoName = process.arv[2];
+const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/FransJoakim/TV-program-guide-Norigin ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) process.exit(code - 1);
+if (!checkedOut) process.exit(-1);
 
 console.log(`Ìnstalling dependencies for ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(code - 1);
+if (!installedDeps) process.exit(-1);
 
 console.log(
-  "Congratulations! You are ready to try my EPG. Execute the following commands in two different terminal windows to start"
+  "Congratulations! You are ready to try my EPG. Execute the following commands in two different terminal windows"
 );
 console.log(`cd ${repoName} && npm start:configured-api`);
 console.log(`cd ${repoName} && npm start`);
